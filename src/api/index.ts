f40ab62e3d8dd8209149017db585d052405a35bc
Hieu Minh To <hieuminh1198@gmail.com>
2021-05-23 22:18:29 +0700

    fix: handle error

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,3 +4,5 @@ export const getPopularEventsByCat = (cat: string) =>
   `${apiRoot}/popular/event_ids/sport/${cat}/`
 
 export const getEventDetail = (id: number) => `${apiRoot}/events/${id}/`
+
+export const ERROR_MESSAGE = 'Something went wrong'
--- src/reducer/events/actions.ts
+++ src/reducer/events/actions.ts
@@ -1,3 +1,4 @@
+import { message } from 'antd'
 import axios from 'axios'
 import { Dispatch } from 'redux'
 import * as api from '../../api'
@@ -5,52 +6,63 @@ import { EVENT_FETCH_FINISH, EVENT_FETCH_START } from './constants'
 
 export function fetchPopularEvents(cat: string) {
   return async (dispatch: Dispatch) => {
-    dispatch({ type: 'EVENT_FETCH_POPULAR_START' })
+    try {
+      dispatch({ type: 'EVENT_FETCH_POPULAR_START' })
 
-    const response = await axios.get(api.getPopularEventsByCat(cat))
-    const { popular_event_ids } = response.data as {
-      popular_event_ids: number[]
-    }
-    dispatch({
-      type: 'EVENT_FETCH_POPULAR_FINISH',
-      data: {
-        cat,
-        value: popular_event_ids,
-      },
-    })
-
-    popular_event_ids.forEach(async (id) => {
+      const response = await axios.get(api.getPopularEventsByCat(cat))
+      const { popular_event_ids } = response.data as {
+        popular_event_ids: number[]
+      }
       dispatch({
-        type: EVENT_FETCH_START,
-        data: { id },
+        type: 'EVENT_FETCH_POPULAR_FINISH',
+        data: {
+          cat,
+          value: popular_event_ids,
+        },
       })
 
-      const eventData = await axios.get(api.getEventDetail(id))
-
-      if (eventData.data.events && eventData.data.events[0]) {
+      popular_event_ids.forEach(async (id) => {
         dispatch({
-          type: EVENT_FETCH_FINISH,
-          data: { id, value: eventData.data.events[0] },
+          type: EVENT_FETCH_START,
+          data: { id },
         })
-      }
-    })
+
+        const eventData = await axios.get(api.getEventDetail(id))
+
+        if (eventData.data.events && eventData.data.events[0]) {
+          dispatch({
+            type: EVENT_FETCH_FINISH,
+            data: { id, value: eventData.data.events[0] },
+          })
+        }
+      })
+    } catch {
+      message.error(api.ERROR_MESSAGE)
+    }
   }
 }
 
 export function fetchEventInfo(id: number) {
   return async (dispatch: Dispatch) => {
-    dispatch({
-      type: EVENT_FETCH_START,
-      data: { id },
-    })
-
-    const eventData = await axios.get(api.getEventDetail(id))
-
-    if (eventData.data.events && eventData.data.events[0]) {
+    try {
       dispatch({
-        type: EVENT_FETCH_FINISH,
-        data: { id, value: eventData.data.events[0] },
+        type: EVENT_FETCH_START,
+        data: { id },
       })
+      try {
+        const eventData = await axios.get(api.getEventDetail(id))
+
+        if (eventData.data.events && eventData.data.events[0]) {
+          dispatch({
+            type: EVENT_FETCH_FINISH,
+            data: { id, value: eventData.data.events[0] },
+          })
+        }
+      } catch {
+        message.error(api.ERROR_MESSAGE)
+      }
+    } catch {
+      message.error(api.ERROR_MESSAGE)
     }
   }
 }
